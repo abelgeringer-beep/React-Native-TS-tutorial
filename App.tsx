d2d090498112dc09a46465e8ebfae83610b3224b
abelgeringer-beep <abelgeringer@gmail.com>
2023-03-17 15:33:23 +0100

    initial commit for react native ts tutorial

--- App.tsx
+++ App.tsx
@@ -2,6 +2,7 @@ import { NavigationContainer } from "@react-navigation/native";
 import { TailwindProvider } from "tailwind-rn";
 import RootNavigator from "./navigator/RootNavigator";
 import utilities from "./tailwind.json";
+import React from "react";
 
 export default function App() {
     return (
